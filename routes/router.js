efafe1b2ada8664c2ecf06cd69ee3f0e9423f51a
LHX <l973749104@gmail.com>
2018-05-02 18:17:26 +0800

    设置路由

--- routes/router.js
+++ routes/router.js
@@ -5,6 +5,20 @@ const router = express.Router();
 
 // 设置路由
 
+/** 
+ * 登录
+*/
+router.post('/login', (req, res) => {
+
+}); 
+
+/** 
+ * 登出
+*/
+router.post('/loginOut', (req, res) => {
+
+});
+
 /**
  * 注册用户信息
  */
@@ -36,7 +50,42 @@ router.get('/getHotData', (req, res) => {
 /**
  * 分页获取主页最新数据
  */
-router.get('path', (req, res) => {
+router.get('/getNewData', (req, res) => {
+
+});
+
+/** 
+ * 搜索文章
+*/
+router.get('/searchArticle', (req, res) => {
+
+});
+
+/** 
+ * 添加关注
+*/
+router.get('/addAttention', (req, res) => {
+
+});
+
+/** 
+ * 移除关注
+*/
+router.get('/removeAttention', (req, res) => {
+
+});
+
+/** 
+ * 添加收藏
+*/
+router.get('/addCollect', (req, res) => {
+
+});
+
+/** 
+ * 移除收藏
+*/
+router.get('/removeCollect', (req, res) => {
 
 });
 
@@ -48,5 +97,57 @@ router.get('/getArticle', (req, res) => {
 });
 
 /** 
- * 
+ * 查看我的文章
 */
+router.get('/myArticle', (req, res) => {
+
+}); 
+
+/** 
+ * 新增文章
+*/
+router.post('/addArticle', (req, res) => {
+
+}); 
+
+/** 
+ * 删除文章
+*/
+router.post('/removeArticle', (req, res) => {
+
+}); 
+
+/** 
+ * 修改文章
+*/
+router.post('/editArticle', (req, res) => {
+
+}); 
+
+/** 
+ * 查看我的收藏
+*/
+router.get('/myCollect', (req, res) => {
+
+}); 
+
+/** 
+ * 查看我的关注
+*/
+router.get('/myAttention', (req, res) => {
+
+}); 
+
+/** 
+ * 上传头像
+*/
+router.get('/uploadAvatar', (req, res) => {
+
+}); 
+
+/** 
+ * 上传文章图片
+*/
+router.get('/uploadArticleImg', (req, res) => {
+
+}); 

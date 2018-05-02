3796ad72593bcff0f19dba6e01177b441060a03e
LHX <l973749104@gmail.com>
2018-05-02 18:03:16 +0800

    新建

---
+++ routes/router.js
@@ -0,0 +1,52 @@
+// 路由Api文件
+
+const express = require('express');
+const router = express.Router();
+
+// 设置路由
+
+/**
+ * 注册用户信息
+ */
+router.get('/saveUserInfo', (req, res) => {
+
+});
+
+/**
+ * 获取用户信息
+ */
+router.get('/getUserInfo', (req, res) => {
+
+});
+
+/**
+ * 修改用户信息
+ */
+router.post('/editInfo', (req, res) => {
+
+});
+
+/**
+ * 分页获取主页热门数据
+ */
+router.get('/getHotData', (req, res) => {
+
+});
+
+/**
+ * 分页获取主页最新数据
+ */
+router.get('path', (req, res) => {
+
+});
+
+/** 
+ * 获取文章详细信息
+*/
+router.get('/getArticle', (req, res) => {
+
+});
+
+/** 
+ * 
+*/
